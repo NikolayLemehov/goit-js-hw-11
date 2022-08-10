584c5b9624cb4c74bdc673dd38bfa27ff8d1dd5a
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-10 17:03:40 +0300

    add notify

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getSearchImage, ITEMS_PER_PAGE } from './api';
 import { photoCardTemplate } from './templates/photo-card.template';
+import { Notify } from 'notiflix';
 
 let gallery = null;
 let onClickLoadMoreBtn = null;
@@ -20,23 +21,29 @@ refs.form.addEventListener('submit', onSubmitForm);
 async function onSubmitForm(e) {
   e.preventDefault();
 
-  removeChildren(refs.gallery)
+  removeChildren(refs.gallery);
   const inputValue = refs.formInput.value.trim();
   // const inputValue = 'towel';
   if (inputValue === '') {
-    return;
+    Notify.warning(`Enter, please, any value in the field.`);
+
+    return false;
   }
-  loadingImages(1, inputValue)().catch(console.log)
+  return loadingImages(1, inputValue)();
 }
 
 function loadingImages(page, value) {
   return async () => {
-    if (onClickLoadMoreBtn) {
-      refs.loadMoreBtn.removeEventListener('click', onClickLoadMoreBtn);
-      onClickLoadMoreBtn = null;
-    }
+    removeMoreEvent(onClickLoadMoreBtn);
+
     try {
       const data = await getSearchImage({ page, value });
+      if (data.totalHits === 0 && page === 1) {
+        Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
+      }
+      if (page === 1) {
+        Notify.info(`Hooray! We found ${data.totalHits} images.`);
+      }
       renderGalleryList(data.hits);
 
       const isVisibleBtn = getIsVisibleLoadMoreBtn({
@@ -47,23 +54,34 @@ function loadingImages(page, value) {
 
       if (isVisibleBtn) {
         refs.loadMoreBtn.classList.remove('is-hidden');
-        onClickLoadMoreBtn = loadingImages(page + 1, value)
+        onClickLoadMoreBtn = loadingImages(page + 1, value);
         refs.loadMoreBtn.addEventListener('click', onClickLoadMoreBtn,
           { once: true });
       } else {
         refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.info(`We're sorry, but you've reached the end of search results.`);
       }
-    } catch {}
+    } catch (e) {
+      Notify.failure(e.message);
+    }
+  };
+}
+
+function removeMoreEvent(onEvent) {
+  if (onEvent) {
+    refs.loadMoreBtn.removeEventListener('click', onEvent);
+    onEvent = null;
   }
 }
 
 function renderGalleryList(images) {
-  const templates = images.map(img => photoCardTemplate(img)).join('')
+  const templates = images.map(img => photoCardTemplate(img)).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', templates);
   if (gallery) {
-    gallery.destroy();
+    gallery.refresh();
+  } else {
+    gallery = new SimpleLightbox('.gallery a');
   }
-  refs.gallery.insertAdjacentHTML('beforeend', templates)
-  gallery = new SimpleLightbox('.gallery a');
 }
 
 function removeChildren(el) {
